a4e9bf1b8754a34aa4d34f4eef2bab3db3b8560e
Sebastian Garcia <sebastian.segarcia@gmail.com>
2026-02-13 08:38:32 -0600

    Add files via upload

---
+++ SQLgen.py
@@ -0,0 +1,64 @@
+import random
+import pandas 
+NUM_WEEKS = 52
+SALES_AMOUNT = 1000000
+NUM_PEAKS = 3
+NUM_MENU_ITEMS = 20
+NUM_REQ_QUERIES  = 15
+
+
+
+def generateWeeklySales():
+    """
+    
+    """
+    for _ in range()
+
+
+    
+
+
+def generatePeakSales():
+    # Select top 10 sums of order total grouped by
+    # day in desce
+
+
+
+
+
+
+
+#special query 3
+#
+#given a specific hour of the day, how many order were placed and what was the total sum of the orders
+def generateRealisticSalesHistory():
+    
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "main":
+    print("tuff")
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
